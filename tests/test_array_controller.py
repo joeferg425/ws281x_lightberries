65c187375cbd4a4aa009339c1e4cb9472bc41fca
jf <jokester425@gmail.com>
2022-05-13 20:09:51 -0700

    removed logging calls

--- tests/test_array_controller.py
+++ tests/test_array_controller.py
@@ -79,14 +79,14 @@ def newController() -> ArrayController:
 
 def test_create():
     with mock.patch.object(ArrayController, "_instantiate_WS281xString", new_instantiate_WS281xString):
-        ac = ArrayController(debug=True, verbose=True)
+        ac = ArrayController()
         assert ac is not None
         assert isinstance(ac.ws281xString, WS281xString)
 
 
 def test_properties():
     with mock.patch.object(ArrayController, "_instantiate_WS281xString", new_instantiate_WS281xString):
-        ac = ArrayController(debug=True, verbose=True)
+        ac = ArrayController()
         assert isinstance(ac.refreshDelay, float)
         ac.refreshDelay = 0.1
         assert isinstance(ac.backgroundColor, np.ndarray)
@@ -110,7 +110,7 @@ def test_properties():
 
 def test_delete():
     with mock.patch.object(ArrayController, "_instantiate_WS281xString", new_instantiate_WS281xString):
-        ac = ArrayController(debug=True, verbose=True)
+        ac = ArrayController()
         assert isinstance(ac.ws281xString, WS281xString)
         ac.__del__()
         assert ac.ws281xString is None
@@ -118,7 +118,7 @@ def test_delete():
 
 def test_reset():
     with mock.patch.object(ArrayController, "_instantiate_WS281xString", new_instantiate_WS281xString):
-        ac = ArrayController(debug=True, verbose=True)
+        ac = ArrayController()
         ac.setvirtualLEDBuffer(ConvertPixelArrayToNumpyArray([PixelColors.OFF]))
         ac.reset()
         assert len(ac.virtualLEDBuffer) == ac.realLEDCount
@@ -126,14 +126,14 @@ def test_reset():
 
 def test_refresh_callback():
     with mock.patch.object(ArrayController, "_instantiate_WS281xString", new_instantiate_WS281xString):
-        ac = ArrayController(debug=True, verbose=True)
+        ac = ArrayController()
         ac.refreshCallback = print
         ac.refreshLEDs()
 
 
 def test_getRandomIndices():
     with mock.patch.object(ArrayController, "_instantiate_WS281xString", new_instantiate_WS281xString):
-        ac = ArrayController(debug=True, verbose=True)
+        ac = ArrayController()
         for i in range(3):
             temp = ac.getRandomIndices(i)
             assert len(temp) == i
@@ -143,5 +143,5 @@ def test_getRandomIndices():
 
 def test_getRandomBoolean():
     with mock.patch.object(ArrayController, "_instantiate_WS281xString", new_instantiate_WS281xString):
-        ac = ArrayController(debug=True, verbose=True)
+        ac = ArrayController()
         assert isinstance(ac.getRandomBoolean(), bool)
